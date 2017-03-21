ba6f4e1d4cebcebedd32d8e01561d9f4a467d5d3
marski <martin.vidjeskog@hotmail.com>
2017-03-21 12:54:49 +0200

    Staattiset HTML sivut lisätty

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -4,11 +4,58 @@
 
     public static function index(){
       // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+   	  View::make('suunnitelmat/frontPage.html');
     }
 
     public static function sandbox(){
       // Testaa koodiasi täällä
-      echo 'Hello World!';
+        //echo 'Hello World!';
+        View::make('helloworld.html');
     }
+    
+    public static function recipe_list(){
+      // Renderöidään reseptilistasivu
+        View::make('suunnitelmat/recipe_list.html');
+    }
+    
+    public static function recipe_show(){
+      // Renderöidään reseptin esittely sivu
+        View::make('suunnitelmat/recipe_show.html');
+    }
+    
+    public static function sign_in(){
+      // Renderöidään sisäänkirjautumis sivu
+        View::make('suunnitelmat/sign_in.html');
+    }
+    
+    public static function sign_up(){
+      // Renderöidään rekisteröitymis sivu
+        View::make('suunnitelmat/sign_up.html');
+    }
+    
+    public static function change_settings(){
+      // Renderöidään käyttäjän asetusten muutos sivu
+        View::make('suunnitelmat/account_settings.html');
+    }
+    
+    public static function recipe_suggest(){
+      // Renderöidään reseptin ehdotus sivu
+        View::make('suunnitelmat/recipe_suggest.html');
+    }
+    
+    public static function suggest_list(){
+      // Renderöidään reseptin ehdotus sivu
+        View::make('suunnitelmat/suggest_list.html');
+    }
+    
+    public static function user_list(){
+      // Renderöidään reseptin ehdotus sivu
+        View::make('suunnitelmat/user_list.html');
+    }
+    
+    public static function recipi_edit(){
+      // Renderöidään reseptin ehdotus sivu
+        View::make('suunnitelmat/recipe_edit.html');
+    }
+    
   }
--- config/routes.php
+++ config/routes.php
@@ -7,3 +7,39 @@
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
+  
+  $routes->get('/recipi', function() {
+    HelloWorldController::recipe_list();
+  });
+  
+  $routes->get('/recipi/1', function() {
+    HelloWorldController::recipe_show();
+  });
+  
+  $routes->get('/login', function() {
+    HelloWorldController::sign_in();
+  });
+  
+  $routes->get('/register', function() {
+    HelloWorldController::sign_up();
+  });
+  
+  $routes->get('/settings', function() {
+    HelloWorldController::change_settings();
+  });
+  
+  $routes->get('/suggest', function() {
+    HelloWorldController::recipe_suggest();
+  });
+  
+  $routes->get('/suggestion', function() {
+    HelloWorldController::suggest_list();
+  });
+  
+  $routes->get('/user', function() {
+    HelloWorldController::user_list();
+  });
+  
+  $routes->get('/recipe/1/edit', function() {
+    HelloWorldController::recipi_edit();
+  });
